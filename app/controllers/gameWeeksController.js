cf950092a0936d8ef271e02f1e5c5474986c6f86
amygoldberg <amykgoldberg@gmail.com>
2015-07-16 19:36:41 -0400

    updating admin creating new game

--- app/controllers/gameWeeksController.js
+++ app/controllers/gameWeeksController.js
@@ -1,24 +1,23 @@
 (function gameWeeksController() {
 
+  var makeBlankGameWeek = function(weekNumber) {
+    var blankWeek = {};
+    blankWeek.game = {};
+    blankWeek.points = "";
+    blankWeek.week_number = weekNumber;
+    return blankWeek;
+  };
+
   var GameWeeksController = function($routeParams, gameWeeksFactory) {
     var vm = this;
     vm.userId = $routeParams.userId;
     vm.user = gameWeeksFactory.user;
     vm.gameWeeks = gameWeeksFactory.gameWeeks;
-    this.newGameWeekForm = {};
-    this.newGameWeekForm.game = {};
-    this.newGameWeekForm.points = "";
-    this.newGames = [];
-
-    this.createNewGame = function() {
-      this.newGames.push(this.newGameWeekForm.game);
-    }
-
-    this.createGameWeek = function() {
-      gameWeeksFactory.createGameWeek(this.newGames);
-    }
-
-    this.newGameWeekForm.teams = [{
+    this.newGameWeekForm = makeBlankGameWeek(vm.gameWeeks.length + 1);
+    // this.newGameWeekForm = {};
+    // this.newGameWeekForm.game = {};
+    // this.newGameWeekForm.points = "";
+    this.teams = [{
       name: "arizona cardinals"
     }, {
       name: "atlanta falcons"
@@ -83,8 +82,7 @@
     }, {
       name: "washington redskins"
     }];
-
-    this.newGameWeekForm.spreads = [{
+    this.spreads = [{
       points: "-1"
     }, {
       points: "-1.5"
@@ -125,6 +123,127 @@
     }, {
       points: "-10.5"
     }];
+    this.newGames = [];
+
+    this.createNewGame = function() {
+      this.newGames.push(this.newGameWeekForm.game);
+      this.newGameWeekForm.game = {};
+      // this.newGameWeekForm = {};
+      // this.newGameWeekForm.game = {};
+      // this.newGameWeekForm.points = "";
+    }
+
+    this.createGameWeek = function() {
+      gameWeeksFactory.createGameWeek(this.newGames);
+    }
+
+    // this.newGameWeekForm.teams = [{
+    //   name: "arizona cardinals"
+    // }, {
+    //   name: "atlanta falcons"
+    // }, {
+    //   name: "baltimore ravens"
+    // }, {
+    //   name: "buffalo bills"
+    // }, {
+    //   name: "carolina panthers"
+    // }, {
+    //   name: "chicago bears"
+    // }, {
+    //   name: "cincinnati bengals"
+    // }, {
+    //   name: "cleveland browns"
+    // }, {
+    //   name: "dallas cowboys"
+    // }, {
+    //   name: "denver broncos"
+    // }, {
+    //   name: "detroit lions"
+    // }, {
+    //   name: "green bay packers"
+    // }, {
+    //   name: "houston texans"
+    // }, {
+    //   name: "indianapolis colts"
+    // }, {
+    //   name: "jacksonville jaguars"
+    // }, {
+    //   name: "kansas city chiefs"
+    // }, {
+    //   name: "miami dolphins"
+    // }, {
+    //   name: "minnesota vikings"
+    // }, {
+    //   name: "new england patriots"
+    // }, {
+    //   name: "new orleans saints"
+    // }, {
+    //   name: "new york giants"
+    // }, {
+    //   name: "new york jets"
+    // }, {
+    //   name: "oakland raiders"
+    // }, {
+    //   name: "philadelphia eagles"
+    // }, {
+    //   name: "pittsburgh steelers"
+    // }, {
+    //   name: "san diego chargers"
+    // }, {
+    //   name: "san fran 49ers"
+    // }, {
+    //   name: "seattle seahawks"
+    // }, {
+    //   name: "st. louis rams"
+    // }, {
+    //   name: "tampa bay buccaneers"
+    // }, {
+    //   name: "tennessee titans"
+    // }, {
+    //   name: "washington redskins"
+    // }];
+
+    // this.newGameWeekForm.spreads = [{
+    //   points: "-1"
+    // }, {
+    //   points: "-1.5"
+    // }, {
+    //   points: "-2"
+    // }, {
+    //   points: "-2.5"
+    // }, {
+    //   points: "-3"
+    // }, {
+    //   points: "-3.5"
+    // }, {
+    //   points: "-4"
+    // }, {
+    //   points: "-4.5"
+    // }, {
+    //   points: "-5"
+    // }, {
+    //   points: "-5.5"
+    // }, {
+    //   points: "-6"
+    // }, {
+    //   points: "-6.5"
+    // }, {
+    //   points: "-7"
+    // }, {
+    //   points: "-7.5"
+    // }, {
+    //   points: "-8"
+    // }, {
+    //   points: "-8.5"
+    // }, {
+    //   points: "-9"
+    // }, {
+    //   points: "-9.5"
+    // }, {
+    //   points: "-10"
+    // }, {
+    //   points: "-10.5"
+    // }];
 
     function init() {
       gameWeeksFactory.getGameWeeks();
