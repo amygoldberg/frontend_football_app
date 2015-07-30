5bcf4b45ec92ecf58fc8e89dde697ccc46ca06f8
amygoldberg <amykgoldberg@gmail.com>
2015-07-30 17:41:36 -0400

    fixing rack cors issue

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('footballApp', ['ngRoute', 'appRoutes']);
 
 app.run(function($http, $window) {
+  $http.defaults.useXDomain = true;
+  delete $http.defaults.headers.common['X-Requested-With'];
   var token = $window.localStorage.getItem('token');
   if (token) {
     $http.defaults.headers.common.Authorization = 'Token token=' + token;
